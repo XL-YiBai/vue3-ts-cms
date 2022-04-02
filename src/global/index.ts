841c52c4454a725501dc1a19f16be1deef9d7257
leishao06 <987313405@qq.com>
2022-04-02 23:28:36 +0800

    Element-plus安装插件形式引入

--- src/global/index.ts
+++ src/global/index.ts
@@ -2,7 +2,7 @@
 import { App } from 'vue'
 import registerElement from './register-element'
 
-export function registerApp(app: App): void {
-  // 全局注册需要用到的Element-plus组件
-  registerElement(app)
+export function globalregisterApp(app: App): void {
+  // 全局注册需要用到的Element-plus组件，使用app.use会自动执行传入函数，并给这个函数传入app实例。
+  app.use(registerElement)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 
-import { registerApp } from './global/index'
+import { globalregisterApp } from './global/index'
 
 import App from './App.vue'
 
@@ -10,7 +10,8 @@ import store from './store'
 const app = createApp(App)
 
 // 注册一些全局的东西
-registerApp(app)
+
+app.use(globalregisterApp) // 使用app.use会自动执行传入函数，并给这个函数传入app实例。这里也可以直接写globalregisterApp(app)
 
 app.use(router)
 app.use(store)
