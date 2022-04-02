995f424eb3fa25e323b132aaa69de9ff0cd0ff12
leishao06 <987313405@qq.com>
2022-04-02 20:01:44 +0800

    集成vue-router

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+// const path = require('path')
+
+module.exports = {
+  // 1. 配置方式一：CLI 提供的属性
+  outputDir: './build'
+  // 2. 配置方式二：和webpack属性完全一致，最后会进行合并
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/components'
+  //     }
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     views: '@/views'
+  //   }
+  // },
+  // chainWebpack: (config) => {
+  //   config.resolve.alias.set('@', path.resolve(__dirname, 'src')).set('views', '@/views')
+  // }
+}
