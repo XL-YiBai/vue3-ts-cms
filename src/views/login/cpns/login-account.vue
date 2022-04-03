8ca10c4a13f5a66b804ad462bdd226dc8bad548a
leishao06 <987313405@qq.com>
2022-04-03 21:07:58 +0800

    抽离校验规则到config文件

--- src/views/login/cpns/login-account.vue
+++ src/views/login/cpns/login-account.vue
@@ -13,6 +13,7 @@
 
 <script lang="ts">
 import { defineComponent, reactive } from 'vue'
+import { rules } from '../config/account.confg'
 
 export default defineComponent({
   setup() {
@@ -20,34 +21,6 @@ export default defineComponent({
       name: '',
       password: ''
     })
-
-    // 编写规则
-    const rules = {
-      name: [
-        {
-          required: true,
-          message: '用户名是必传内容~',
-          trigger: 'blur'
-        },
-        {
-          pattern: /^[a-z0-9]{5,10}$/,
-          message: '用户名必须是5-10个字母或者数字~',
-          trigger: 'blur'
-        }
-      ],
-      password: [
-        {
-          required: true,
-          message: '密码是必传内容~',
-          trigger: 'blur'
-        },
-        {
-          pattern: /^[a-z0-9]{3,}$/,
-          message: '密码必须是3位以上的字母或者数字~',
-          trigger: 'blur'
-        }
-      ]
-    }
 
     return { account, rules }
   }
