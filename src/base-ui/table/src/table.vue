0a1b46cb2ce4a320d2a47565c4d3bb9853b1ba9d
leishao06 <987313405@qq.com>
2022-04-07 00:24:12 +0800

    XlTable的动态插槽和作用域插槽

--- src/base-ui/table/src/table.vue
+++ src/base-ui/table/src/table.vue
@@ -2,7 +2,17 @@
   <div class="xl-table">
     <el-table :data="listData" border style="width: 100%">
       <template v-for="propItem in propList" :key="propItem.prop">
-        <el-table-column v-bind="propItem" align="center" />
+        <el-table-column v-bind="propItem" align="center">
+          <!-- 可以使用插槽在表格插入想传入的内容，这里会通过作用域插槽把每一行的内容传递出来 -->
+          <template #default="scope">
+            <!-- 拿到每一个数据的prop字段，prop指定放在哪一列，对应每一列的列名 -->
+            <!-- 根据是否传入的插槽名，自定义每一列插入的内容，如果没有传插槽名，就默认展示下面的文本 -->
+            <!-- 外界使用时通过指定插槽名自定义每一列内容，这里再通过作用域插槽把内容传递给外界 -->
+            <slot :name="propItem.slotName" :row="scope.row">
+              {{ scope.row[propItem.prop] }}
+            </slot>
+          </template>
+        </el-table-column>
       </template>
     </el-table>
   </div>
@@ -13,10 +23,12 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   props: {
+    // 列表的数据信息
     listData: {
       type: Array,
       required: true
     },
+    // 列表的表头名称配置信息
     propList: {
       type: Array,
       required: true
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -3,7 +3,15 @@
     <PageSearch :searchFormConfig="searchFormConfig"></PageSearch>
 
     <div class="content">
-      <XlTable :listData="userList" :propList="propList"></XlTable>
+      <XlTable :listData="userList" :propList="propList">
+        <!-- 通过具名插槽指定每一列插入内容，通过作用域插槽拿到每一项的内容，从而对数据格式化展示 -->
+        <template #status="scope">
+          <el-button>{{ scope.row.enable ? '启用' : '禁用' }}</el-button>
+        </template>
+        <template #createAt="scope">
+          <el-button>{{ scope.row.createAt }}</el-button>
+        </template>
+      </XlTable>
     </div>
   </div>
 </template>
@@ -21,7 +29,7 @@ export default defineComponent({
   components: { PageSearch, XlTable },
   setup() {
     const store = useStore()
-    // 调用dispatch获取数据，存到vuex
+    // 调用dispatch获取该页面的数据，存到vuex
     store.dispatch('system/getPageListAction', {
       pageUrl: '/users/list',
       queryInfo: {
@@ -33,13 +41,24 @@ export default defineComponent({
     const userList = computed(() => store.state.system.userList)
     const userCount = computed(() => store.state.system.userCount)
 
+    // 每一列的配置信息
     const propList = [
       { prop: 'name', label: '用户名', minwidth: '100' },
       { prop: 'realname', label: '真实姓名', minwidth: '100' },
       { prop: 'cellphone', label: '手机号码', minwidth: '100' },
-      { prop: 'enable', label: '状态', minwidth: '100' },
-      { prop: 'createAt', label: '创建时间', minwidth: '250' },
-      { prop: 'updateAt', label: '更新时间', minwidth: '250' }
+      { prop: 'enable', label: '状态', minwidth: '100', slotName: 'status' },
+      {
+        prop: 'createAt',
+        label: '创建时间',
+        minwidth: '250',
+        slotName: 'createAt'
+      },
+      {
+        prop: 'updateAt',
+        label: '更新时间',
+        minwidth: '250',
+        slotName: 'updateAt'
+      }
     ]
 
     return { searchFormConfig, userList, propList }
