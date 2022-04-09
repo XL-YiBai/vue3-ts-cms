d1cd9317b0c2f7bf72596fce5184355b918f7f6c
leishao06 <987313405@qq.com>
2022-04-09 23:45:57 +0800

    角色页面的新建和编辑实现

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -4,7 +4,15 @@
     <PageContent
       :contentTableConfig="contentTableConfig"
       pageName="role"
+      @newBtnClick="handleNewData"
+      @editBtnClick="handleEditData"
     ></PageContent>
+    <PageModal
+      ref="pageModalRef"
+      :defaultInfo="defaultInfo"
+      :modalConfig="modalConfig"
+      pageName="role"
+    ></PageModal>
   </div>
 </template>
 
@@ -13,15 +21,29 @@ import { defineComponent } from 'vue'
 
 import PageSearch from '@/components/page-search'
 import PageContent from '@/components/page-content'
+import PageModal from '@/components/page-modal'
 
 import { searchFormConfig } from './config/search.config'
 import { contentTableConfig } from './config/content.config'
+import { modalConfig } from './config/modal.config'
+
+import { usePageModal } from '@/hooks/use-page-modal'
 
 export default defineComponent({
   name: 'role',
-  components: { PageSearch, PageContent },
+  components: { PageSearch, PageContent, PageModal },
   setup() {
-    return { searchFormConfig, contentTableConfig }
+    const [pageModalRef, defaultInfo, handleNewData, handleEditData] =
+      usePageModal()
+    return {
+      searchFormConfig,
+      contentTableConfig,
+      modalConfig,
+      pageModalRef,
+      defaultInfo,
+      handleNewData,
+      handleEditData
+    }
   }
 })
 </script>
